fda58528d311453a155edb95775297dc1e1f0762
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2024-05-04 21:51:02 +0100

    finished narrative tests asked. Create AddressFinder, reverting tests operations still undone

---
+++ src/main/java/webapp/persistence/AddressFinder.java
@@ -0,0 +1,5 @@
+package webapp.persistence;
+
+public class AddressFinder {
+
+}
--- src/test/java/vvs_webapp/WebAppNarrativeHtmlUnitTest.java
+++ src/test/java/vvs_webapp/WebAppNarrativeHtmlUnitTest.java
@@ -1,6 +1,8 @@
 package vvs_webapp;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.*;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
@@ -74,23 +76,8 @@ public class WebAppNarrativeHtmlUnitTest {
 		final String POSTAL_CODE = "postal-code";
 		final String LOCALITY = "locality";
 		
-		HtmlAnchor addAddressToCustomerLink = page.getAnchorByHref("addAddressToCustomer.html");
-		nextPage = (HtmlPage) addAddressToCustomerLink.openLinkInNewWindow();
-		HtmlForm addAdressForms = nextPage.getForms().get(0);
-		vatInput = addAdressForms.getInputByName("vat");
-		vatInput.setValueAttribute(VAT);
-		HtmlInput addressInput = addAdressForms.getInputByName("address");
-		HtmlInput doorInput = addAdressForms.getInputByName("door");
-		HtmlInput postalCodeInput = addAdressForms.getInputByName("postalCode");
-		HtmlInput localityInput = addAdressForms.getInputByName("locality");
-		submit = addAdressForms.getInputByValue("Insert");
-		
-		for(int i = 0; i<2; i++) {
-			addressInput.setValueAttribute(ADDRESS+i);
-			doorInput.setValueAttribute(DOOR+i);
-			postalCodeInput.setValueAttribute(POSTAL_CODE+i);
-			localityInput.setValueAttribute(LOCALITY+i);
-			reportPage = submit.click();
+		for(int i = 0; i < 2; i++) {
+			reportPage = createAdress(VAT,ADDRESS+i, DOOR+i, POSTAL_CODE+i, LOCALITY+i);
 		}
 		
 		HtmlTable addressesTable = (HtmlTable) reportPage.getByXPath("//table").get(0);
@@ -110,6 +97,24 @@ public class WebAppNarrativeHtmlUnitTest {
 		//TODO: /////////////////////// REVERT /////////////////////////
 	}
 	
+	private HtmlPage createAdress(String vat, String address, String door, String postalCode, String locality) throws IOException {
+		HtmlAnchor addAddressToCustomerLink = page.getAnchorByHref("addAddressToCustomer.html");
+		HtmlPage nextPage = (HtmlPage) addAddressToCustomerLink.openLinkInNewWindow();
+		HtmlForm addAdressForms = nextPage.getForms().get(0);
+		HtmlInput vatInput = addAdressForms.getInputByName("vat");
+		vatInput.setValueAttribute(vat);
+		HtmlInput addressInput = addAdressForms.getInputByName("address");
+		HtmlInput doorInput = addAdressForms.getInputByName("door");
+		HtmlInput postalCodeInput = addAdressForms.getInputByName("postalCode");
+		HtmlInput localityInput = addAdressForms.getInputByName("locality");
+		HtmlInput submit = addAdressForms.getInputByValue("Insert");
+		addressInput.setValueAttribute(address);
+		doorInput.setValueAttribute(door);
+		postalCodeInput.setValueAttribute(postalCode);
+		localityInput.setValueAttribute(locality);
+		return submit.click();
+	}
+
 	// b)
 	@Test
 	public void insertTwoCustomersAndCheckInListAllCustomersTest() throws IOException {
@@ -122,27 +127,13 @@ public class WebAppNarrativeHtmlUnitTest {
 		final String PHONE2 = "2";
 		
 		// 1. Add customers
-		HtmlAnchor addCustomerLink = page.getAnchorByHref("addCustomer.html");
-		HtmlPage nextPage = (HtmlPage) addCustomerLink.openLinkInNewWindow();
-		HtmlForm addCustomerForm = nextPage.getForms().get(0);
-
-		HtmlInput vatInput = addCustomerForm.getInputByName("vat");
-		vatInput.setValueAttribute(VAT1);
-		HtmlInput designationInput = addCustomerForm.getInputByName("designation");
-		designationInput.setValueAttribute(DESIG1);
-		HtmlInput phoneInput = addCustomerForm.getInputByName("phone");
-		phoneInput.setValueAttribute(PHONE1);
-		HtmlInput submit = addCustomerForm.getInputByName("submit");
-		HtmlPage reportPage = submit.click();
+		HtmlPage reportPage= createCustomer(VAT1,DESIG1,PHONE1);
 		
-		vatInput.setValueAttribute(VAT2);
-		designationInput.setValueAttribute(DESIG2);
-		phoneInput.setValueAttribute(PHONE2);
-		reportPage = submit.click();
+		reportPage = createCustomer(VAT2,DESIG2,PHONE2);
 		
 		// 2. Check List All Customers
 		HtmlAnchor listAllCustomersLink = page.getAnchorByHref("GetAllCustomersPageController");
-		nextPage = (HtmlPage) listAllCustomersLink.openLinkInNewWindow();
+		HtmlPage nextPage = (HtmlPage) listAllCustomersLink.openLinkInNewWindow();
 		String textAllCustomers = nextPage.asText();
 		
 		assertTrue(textAllCustomers.contains(VAT1));
@@ -155,37 +146,47 @@ public class WebAppNarrativeHtmlUnitTest {
 		
 		
 		///////////////////////////// REVERT //////////////////////////////////////////
-		HtmlAnchor removeCustomerLink = page.getAnchorByHref("RemoveCustomerPageController");
-		nextPage = (HtmlPage) removeCustomerLink.openLinkInNewWindow();
-		HtmlForm removeCustomerForm = nextPage.getForms().get(0);
-		
-		vatInput = removeCustomerForm.getInputByName("vat");
-		vatInput.setValueAttribute(VAT1);
-		
-		submit = removeCustomerForm.getInputByName("submit");
-		reportPage = submit.click();
-		vatInput.setValueAttribute(VAT2);
-		reportPage = submit.click();
+		reportPage = removeCustomer(VAT1);
+		reportPage = removeCustomer(VAT2);
 		// TODO: Assert para garantir que apaguei os customers?
 		
 	}
 	
+	private HtmlPage removeCustomer(String vat) throws IOException {
+		HtmlAnchor removeCustomerLink = page.getAnchorByHref("RemoveCustomerPageController");
+		HtmlPage nextPage = (HtmlPage) removeCustomerLink.openLinkInNewWindow();
+		HtmlForm removeCustomerForm = nextPage.getForms().get(0);
+		
+		HtmlInput vatInput = removeCustomerForm.getInputByName("vat");
+		vatInput.setValueAttribute(vat);
+		
+		HtmlInput submit = removeCustomerForm.getInputByName("submit");
+		return submit.click();
+	}
+
+	private HtmlPage createCustomer(String vat, String designation, String phone) throws IOException {
+		HtmlAnchor addCustomerLink = page.getAnchorByHref("addCustomer.html");
+		HtmlPage nextPage = (HtmlPage) addCustomerLink.openLinkInNewWindow();
+		HtmlForm addCustomerForm = nextPage.getForms().get(0);
+
+		HtmlInput vatInput = addCustomerForm.getInputByName("vat");
+		vatInput.setValueAttribute(vat);
+		HtmlInput designationInput = addCustomerForm.getInputByName("designation");
+		designationInput.setValueAttribute(designation);
+		HtmlInput phoneInput = addCustomerForm.getInputByName("phone");
+		phoneInput.setValueAttribute(phone);
+		HtmlInput submit = addCustomerForm.getInputByName("submit");
+		return submit.click();
+		
+	}
+
 	//c) e d)
 	@Test
 	public void openSaleForClientAndCloseItTest() throws IOException {
 		final String VAT = "197672337"; // vat of exisiting customer
 		
 		// c) Create a Sale for the client
-		HtmlAnchor addSaleLink = page.getAnchorByHref("addSale.html");
-		HtmlPage nextPage = (HtmlPage) addSaleLink.openLinkInNewWindow();
-		HtmlForm addSaleForm = nextPage.getForms().get(0);
-
-		HtmlInput vatInput = addSaleForm.getInputByName("customerVat");
-		vatInput.setValueAttribute(VAT);
-		HtmlInput submit = addSaleForm.getInputByValue("Add Sale");
-
-		HtmlPage reportPage = submit.click();
-		String textReportPage = reportPage.asText();
+		HtmlPage reportPage = createSale(VAT);
 		
 		// TODO: se ja tiver uma aberta antes do teste, este teste nao serve para nada
 //		assertTrue(textReportPage.contains("O"));
@@ -201,12 +202,12 @@ public class WebAppNarrativeHtmlUnitTest {
 		
 		// d) Close the sale
 		HtmlAnchor closeSaleLink = page.getAnchorByHref("UpdateSaleStatusPageControler");
-		nextPage = (HtmlPage) closeSaleLink.openLinkInNewWindow();
+		HtmlPage nextPage = (HtmlPage) closeSaleLink.openLinkInNewWindow();
 		HtmlForm closeSaleForm = nextPage.getForms().get(0);
 
 		HtmlInput saleIdInput = closeSaleForm.getInputByName("id");
 		saleIdInput.setValueAttribute(SALE_ID);
-		submit = closeSaleForm.getInputByValue("Close Sale");
+		HtmlInput submit = closeSaleForm.getInputByValue("Close Sale");
 
 		reportPage = submit.click();
 		//  Get the table and check that the the intended sale is closed
@@ -218,13 +219,148 @@ public class WebAppNarrativeHtmlUnitTest {
 		assertEquals(VAT, cells.get(4).asText());
 		
 		// TODO: Nao tenho como apagar para reverter
-
-	}
-	
-	// TODO: Check that all intermediate pages have the expected information.
-	@Test
-	public void createCustomerThenSaleThenDeliveryAndShowDeliveryTest() {
 		
 	}
+	
+	private HtmlPage createSale(String vat) throws IOException {
+		HtmlAnchor addSaleLink = page.getAnchorByHref("addSale.html");
+		HtmlPage nextPage = (HtmlPage) addSaleLink.openLinkInNewWindow();
+		HtmlForm addSaleForm = nextPage.getForms().get(0);
+
+		HtmlInput vatInput = addSaleForm.getInputByName("customerVat");
+		vatInput.setValueAttribute(vat);
+		HtmlInput submit = addSaleForm.getInputByValue("Add Sale");
+		return submit.click();
+	}
+
+	@Test
+	public void createCustomerThenSaleThenDeliveryAndShowDeliveryTest() throws IOException {
+		final String VAT = "123456789";
+		final String DESIG = "desgination1";
+		final String PHONE = "1";
+		
+		// 1. Add customers
+		HtmlPage reportPage= createCustomer(VAT,DESIG,PHONE);
+		
+		// 1.1 This page shows the designation and phone
+		String textReportPage = reportPage.asText();
+		assertTrue(textReportPage.contains(DESIG));
+		assertTrue(textReportPage.contains(PHONE));
+		
+		// 2. Create sale
+		reportPage = createSale(VAT);
+		
+		// 2.1 This page shows the table of the sales of this client
+		//		The last row should be the inserted sale
+		HtmlTable salesTable = (HtmlTable) reportPage.getByXPath("//table").get(0);
+		HtmlTableRow latestSale = salesTable.getRow(salesTable.getRowCount() - 1);
+		List<HtmlTableCell> cells = latestSale.getCells();
+		final String SALE_ID= cells.get(0).asText();
+		assertEquals("O", cells.get(3).asText());
+		assertEquals(VAT, cells.get(4).asText());
+		
+		// * To create delivery its needed to have an address
+		final String ADDRESS = "address";
+		final String DOOR = "door";
+		final String POSTAL_CODE = "postal-code";
+		final String LOCALITY = "locality";
+		reportPage = createAdress(VAT,ADDRESS, DOOR, POSTAL_CODE, LOCALITY);
+		
+		// 3. Create a delivery
+		HtmlAnchor createDeliveryLink = page.getAnchorByHref("saleDeliveryVat.html");
+		HtmlPage nextPage = (HtmlPage) createDeliveryLink.openLinkInNewWindow();
+		HtmlForm createDeliveryForm = nextPage.getForms().get(0);
+
+		HtmlInput vatInput = createDeliveryForm.getInputByName("vat");
+		vatInput.setValueAttribute(VAT);
+		HtmlInput submit = createDeliveryForm.getInputByValue("Get Customer");
+		reportPage = submit.click();
+		
+		// 3.1 Get the Ids of the Sale and Address created
+		HtmlTable addressTable = (HtmlTable) reportPage.getByXPath("//table").get(0);
+		salesTable = (HtmlTable) reportPage.getByXPath("//table").get(1);
+		
+		HtmlTableRow latestAddress = addressTable.getRow(addressTable.getRowCount() -1);
+		latestSale = salesTable.getRow(salesTable.getRowCount() -1);
+		List<HtmlTableCell> addressCells = latestAddress.getCells();
+		List<HtmlTableCell> saleCells = latestSale.getCells();
+		
+		assertEquals(ADDRESS ,addressCells.get(1).asText());
+		assertEquals(DOOR ,addressCells.get(2).asText());
+		assertEquals(POSTAL_CODE ,addressCells.get(3).asText());
+		assertEquals(LOCALITY ,addressCells.get(4).asText());
+		
+		assertEquals(SALE_ID ,saleCells.get(0).asText());
+		assertEquals("O" ,saleCells.get(3).asText());
+		assertEquals(VAT ,saleCells.get(4).asText());
+		
+		final String ADDRESS_ID= addressCells.get(0).asText();
+		createDeliveryForm = reportPage.getForms().get(0);
+		HtmlInput addressIdInput = createDeliveryForm.getInputByName("addr_id");
+		addressIdInput.setValueAttribute(ADDRESS_ID);
+		HtmlInput saleIdInput = createDeliveryForm.getInputByName("sale_id");
+		saleIdInput.setValueAttribute(SALE_ID);
+		submit = createDeliveryForm.getInputByValue("Insert");
+		reportPage = submit.click();
+		
+		// 3.2 Verify the delivery is there
+		HtmlTable deliveryTable = (HtmlTable) reportPage.getByXPath("//table").get(0);
+		HtmlTableRow latestDelivery = deliveryTable.getRow(deliveryTable.getRowCount() -1);
+		List<HtmlTableCell> deliveryCells = latestDelivery.getCells();
+		
+		assertEquals(SALE_ID, deliveryCells.get(1).asText());
+		assertEquals(ADDRESS_ID, deliveryCells.get(2).asText());
+		
+		//////////////////////// REVERT ////////////////////////////////
+	}
+	
+	// Test designed to test a possible problem detected while manually testing the app in a browser
+	// Problem: Creating a delivery with a non-existent address id
+	// Result: Test failed
+	@Test
+	public void createSaleAndDeliveryWithWrongAddress() throws IOException {
+		final String VAT = "197672337"; // vat of exisiting customer
+		
+		// 1 Create a Sale for the client
+		HtmlPage reportPage = createSale(VAT);
+		
+		// 2 Create a delivery
+		HtmlAnchor createDeliveryLink = page.getAnchorByHref("saleDeliveryVat.html");
+		HtmlPage nextPage = (HtmlPage) createDeliveryLink.openLinkInNewWindow();
+		HtmlForm createDeliveryForm = nextPage.getForms().get(0);
+
+		HtmlInput vatInput = createDeliveryForm.getInputByName("vat");
+		vatInput.setValueAttribute(VAT);
+		HtmlInput submit = createDeliveryForm.getInputByValue("Get Customer");
+		reportPage = submit.click();
+		
+		HtmlTable salesTable = (HtmlTable) reportPage.getByXPath("//table").get(0);
+		
+		HtmlTableRow latestSale = salesTable.getRow(salesTable.getRowCount() -1);
+		List<HtmlTableCell> saleCells = latestSale.getCells();
+		final String ADDRESS_ID= "-1"; // FAKE ADDRESS ID
+		final String SALE_ID= saleCells.get(0).asText();
+		
+		createDeliveryForm = reportPage.getForms().get(0);
+		HtmlInput addressIdInput = createDeliveryForm.getInputByName("addr_id");
+		addressIdInput.setValueAttribute(ADDRESS_ID);
+		HtmlInput saleIdInput = createDeliveryForm.getInputByName("sale_id");
+		saleIdInput.setValueAttribute(SALE_ID);
+		submit = createDeliveryForm.getInputByValue("Insert");
+		final HtmlPage finalPage = submit.click();
+		
+		////////// IF THIS HAPPENS THE ASSERTION SHOULD FAIL
+		assertThrows(Exception.class, () ->{
+			// 3 Verify the delivery is there
+			HtmlTable deliveryTable = (HtmlTable) finalPage.getByXPath("//table").get(0);
+			HtmlTableRow latestDelivery = deliveryTable.getRow(deliveryTable.getRowCount() -1);
+			List<HtmlTableCell> deliveryCells = latestDelivery.getCells();
+			
+			assertEquals(SALE_ID, deliveryCells.get(1).asText());
+			assertEquals(ADDRESS_ID, deliveryCells.get(2).asText());
+		});
+		//////////////////////////////////////////////////////
+	}
+	
 
 }
